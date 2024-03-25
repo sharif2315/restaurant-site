87df7eeadc90ce117fb7890a2ccee683ae4fe84f
Sharif Ahmed <sharifahmed@Sharifs-Laptop.local>
2024-03-25 17:45:52 +0000

    adding frontend

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-3xl">Restaurant App</h1>
+    </main>
+  );
+}
