6e6d8f264213d6749de4b0b1468fbc0ead78b59b
Bruce Wayne <brucethebatmandev@gmail.com>
2024-05-21 15:58:51 +0100

    updating local docker compose config

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -121,6 +121,12 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+print('DB_NAME', config('DB_NAME'))
+print('DB_USER', config('DB_USER'))
+print('DB_PASSWORD', config('DB_PASSWORD'))
+print('DB_HOST', config('DB_HOST'))
+print('DB_PORT', config('DB_PORT'))
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
